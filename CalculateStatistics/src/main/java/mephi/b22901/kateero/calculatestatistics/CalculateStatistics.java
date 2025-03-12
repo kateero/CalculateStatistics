31114d0399aa4757d0f219e84b8b1d5f775534e1
kateero <katyalime785@gmail.com>
2025-03-12 21:18:38 +0300

    Создание
    Создан проект и подключены зависимости

---
+++ CalculateStatistics/src/main/java/mephi/b22901/kateero/calculatestatistics/CalculateStatistics.java
@@ -0,0 +1,10 @@
+package mephi.b22901.kateero.calculatestatistics;
+
+import org.apache.commons.math3.stat.*;
+
+public class CalculateStatistics {
+
+    public static void main(String[] args) {
+        
+    }
+}
